9b924a027b7c15d8e5ceafd15e3a50a4610d63a0
Ariel Malka <ariel@chronotext.org>
2015-06-28 13:13:03 +0300

    TestingFileSystem1: EMSCRIPTEN: USING preRun AND postRun MODULE-HOOKS...

--- Tests/TestingFileSystem1/pre.js
+++ Tests/TestingFileSystem1/pre.js
@@ -1,1 +1,16 @@
-console.log('prejs executed');
+var fs = require('fs');
+fs.writeFileSync('foobar.txt', 'yeehaw');
+
+var Module = {
+  preRun: function() {
+    FS.mkdir("/src");
+    FS.mount(NODEFS, { root: "." }, "/src");
+    console.log('preRun executed');
+  },
+  postRun: function() {
+    FS.unmount("/src");
+    console.log('postRun executed');
+  },
+};
+
+console.log('pre.js executed');
